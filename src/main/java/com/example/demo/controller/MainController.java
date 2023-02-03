0a56b8c2c9f4fea5d759a26a902883fabd45db9c
dshutrin <dshutrin@mail.ru>
2023-02-03 14:00:17 +1000

    first commit

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,12 +3,23 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 @RestController
 public class MainController {
 
     @GetMapping("/")
     public String hello() {
         return "Hello, Spring!";
+    }
+
+    @GetMapping("/my_var")
+    public String my_func() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date(System.currentTimeMillis());
+        return "Дмитрий " + formatter.format(date);
     }
 
 }
